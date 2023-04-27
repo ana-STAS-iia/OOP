99cdd01bf6d6b358828ee36545530a2760cb660a
ana-STAS-iia <126493232+ana-STAS-iia@users.noreply.github.com>
2023-04-27 10:44:43 +0500

    Add files via upload

---
+++ OOP4_1/CCircle/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace CCircle
+{
+    partial class FormCircles
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.isWorkingWithCtrl = new System.Windows.Forms.CheckBox();
+            this.isMultipleSelection = new System.Windows.Forms.CheckBox();
+            this.pnlPaint = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // isWorkingWithCtrl
+            // 
+            this.isWorkingWithCtrl.AutoSize = true;
+            this.isWorkingWithCtrl.Location = new System.Drawing.Point(31, 417);
+            this.isWorkingWithCtrl.Name = "isWorkingWithCtrl";
+            this.isWorkingWithCtrl.Size = new System.Drawing.Size(66, 21);
+            this.isWorkingWithCtrl.TabIndex = 0;
+            this.isWorkingWithCtrl.Text = "CTRL";
+            this.isWorkingWithCtrl.UseVisualStyleBackColor = true;
+            // 
+            // isMultipleSelection
+            // 
+            this.isMultipleSelection.AutoSize = true;
+            this.isMultipleSelection.Location = new System.Drawing.Point(163, 417);
+            this.isMultipleSelection.Name = "isMultipleSelection";
+            this.isMultipleSelection.Size = new System.Drawing.Size(97, 21);
+            this.isMultipleSelection.TabIndex = 1;
+            this.isMultipleSelection.Text = "MULTIPLE";
+            this.isMultipleSelection.UseVisualStyleBackColor = true;
+            // 
+            // pnlPaint
+            // 
+            this.pnlPaint.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlPaint.Location = new System.Drawing.Point(12, 12);
+            this.pnlPaint.Name = "pnlPaint";
+            this.pnlPaint.Size = new System.Drawing.Size(776, 389);
+            this.pnlPaint.TabIndex = 2;
+            this.pnlPaint.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlPaint_Paint);
+            this.pnlPaint.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlPaint_MouseDown);
+            // 
+            // FormCircles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlPaint);
+            this.Controls.Add(this.isMultipleSelection);
+            this.Controls.Add(this.isWorkingWithCtrl);
+            this.KeyPreview = true;
+            this.Name = "FormCircles";
+            this.Text = "FormCircles";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormCircles_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FormCircles_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox isWorkingWithCtrl;
+        private System.Windows.Forms.CheckBox isMultipleSelection;
+        private System.Windows.Forms.Panel pnlPaint;
+    }
+}
+
---
+++ OOP4_1/CCircle/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CCircle
+{
+    public partial class FormCircles : Form
+    {
+        
+        bool isPressedCtrl = false; //нажат ли ctrl
+        int selectedCircles = 1; //сколько выбрано кругов
+        List<CCircle> circles = new List<CCircle>(); //контейнер кругов
+        public FormCircles()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlPaint_Paint(object sender, PaintEventArgs e) //отрисовка кругов
+        {
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i < circles.Count(); i++)
+            {
+                circles[i].Paint(g);
+            }
+        }
+
+        private void ProcessingCircleWithCtrl(CCircle circle) //обработка нажатия на круг, если ctrl зажат
+        {
+            if (circle.isSelect() && selectedCircles > 1) //если круг и так выделен и выделеенных кругов больше, чем 1, то снимаю с него выделение
+            {
+                circle.Unselect();
+                selectedCircles--;
+            }
+            else if (circle.isSelect() && selectedCircles == 1) //если круг выделен и выделеенных кругов 1, то ничего не меняется
+            {
+            }
+            else if (!circle.isSelect()) //если круг не выделен, то выделяю
+            {
+                circle.Select();
+                selectedCircles++;
+            }
+        }
+
+        private void pnlPaint_MouseDown(object sender, MouseEventArgs e) //обработка нажатия
+        {
+            bool isOnCircle = false; //попало ли на круг
+
+            if (isPressedCtrl && isWorkingWithCtrl.Checked) //если чекбокс ctrl помечен и клавиша зажата
+            {
+                for (int i = circles.Count - 1; i > -1; i--) //прохожусь по контейнеру
+                {
+                    if (isMultipleSelection.Checked) //если могу выделять 2 объекта при их пересечении
+                    {
+                        if (circles[i].isLiesOn(e.X, e.Y))// если попала на круг
+                        {
+                            isOnCircle = true; //отмечаю, что попала
+                            ProcessingCircleWithCtrl(circles[i]); //перехожу на обработчик нажатия с ctrl
+                        }
+                    }
+                    else if (circles[i].isLiesOn(e.X, e.Y)) //если не могу выделять 2 объекта при их пересечении, но по кругу попала
+                    {
+                        isOnCircle = true; //отмечаю, что попала
+                        ProcessingCircleWithCtrl(circles[i]); //перехожу на обработчик нажатия с ctrl
+                        break; //выхожу из цикла
+                    }
+                }
+            }
+            else //если работаю без ctrl
+            {
+                selectedCircles = 0; //обнуляю количество выбранных кругов
+                for (int i = circles.Count - 1; i > -1; i--) //прохожусь по контейнеру
+                {
+                    if (isMultipleSelection.Checked) //если могу выделять 2 объекта при их пересечении
+                    {
+                        if (circles[i].isLiesOn(e.X, e.Y)) //если попала по кругу
+                        {
+                            isOnCircle = true; //отмечаю, что попала
+                            circles[i].Select(); //выделяю круг
+                            selectedCircles++; //прибавляю к счетчику выбранных кругов 1
+                        }
+                        else
+                            circles[i].Unselect(); //иначе снимаю с него выделение
+                    }
+                    else //если не могу выделять 2 объекта при их пересечении
+                    {
+                        if (circles[i].isLiesOn(e.X, e.Y) && !isOnCircle) // если попала по кругу
+                        {
+                            isOnCircle = true; //отмечаю, что попала
+                            circles[i].Select(); //выделяю круг
+                            selectedCircles++; //прибавляю к счетчику выбранных кругов 1
+                        }
+                        else
+                            circles[i].Unselect(); //иначе снимаю выделение
+                    }
+                }
+            }
+
+            if (!isOnCircle) //если яне попала по кругу
+            {
+                circles.Add(new CCircle(e.X, e.Y, 20)); //то добавляю
+                circles[circles.Count - 1].Select(); //выделяю его
+
+                if (isPressedCtrl && isWorkingWithCtrl.Checked) selectedCircles++; //если работаю с ctrl, то прибавляю счетчик
+                else selectedCircles = 1; //иначе выбранных кругов 1
+            }
+
+            pnlPaint.Invalidate();
+        }
+
+        private void FormCircles_KeyDown(object sender, KeyEventArgs e) //обработка нажатия клавиш
+        {
+            if (e.KeyCode == Keys.ControlKey) 
+                isPressedCtrl = true; //нажала на ctrl, отмечаю что ctrl работает
+
+            if (e.KeyCode == Keys.Delete) //если нажала клавишу Delete
+            {
+                for (int i = 0; i < circles.Count; i++) //прохожу по контейнеру
+                    if (circles[i].isSelect()) //если круг выделен, то удаляю его
+                        circles.RemoveAt(i--);
+
+                if (circles.Count > 0) //если кругов больше 0
+                {
+                    circles[circles.Count - 1].Select(); //то выделяю последний оставшийся в контейнерее
+                    selectedCircles = 1; //выбранных кругов 1
+                }
+                else
+                    selectedCircles = 0; //иначе выбранных кругов нет
+                
+                pnlPaint.Invalidate();
+            }
+        }
+
+        private void FormCircles_KeyUp(object sender, KeyEventArgs e) //отжатие клавиши ctrl
+        {
+            if (e.KeyCode == Keys.ControlKey)
+                isPressedCtrl = false;
+        }
+
+    }
+}
+
